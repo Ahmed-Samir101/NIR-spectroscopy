76bcbade91006b487d117d0dbfb2d5ab1b66d485
unknown <ahmedhtml.art@gmail.com>
2025-02-23 13:42:42 +0800

    first commit

---
+++ processing.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df1 = pd.read_csv('datasets/train.csv')
+df2 = pd.read_csv('datasets/test.csv')
+
+print(df1.head())
+
+df1.drop(columns=['序号'], inplace=True)
+df2.drop(columns=['序号'], inplace=True)
+
+df1.to_csv('train.csv', index=False)
+df2.to_csv('test.csv', index=False)
+print("Done")
